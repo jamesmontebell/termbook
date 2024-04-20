f06772839247d1d7550b58c727429ff07b82b04a
James Montebell <100632392+jamesmontebell@users.noreply.github.com>
2024-04-19 21:53:30 -0400

    initial app with authentication and one protected route

---
+++ src/app/protected/page.tsx
@@ -0,0 +1,18 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+export default async function ProtectedRoute() {
+  const session = await getServerSession();
+  if (!session || !session.user) {
+    redirect("/api/auth/signin");
+  }
+
+  return (
+    <div>
+      {session.user.email}
+      This is a protected route.
+      <br />
+      You will only see this if you are authenticated.
+    </div>
+  );
+}
