21bd1a4d492edd4c6a44702175a9909c8bd1dcf4
coleb177 <colebarbes17@gmail.com>
2024-04-20 06:11:10 -0400

    cards for profile page

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,10 @@
+import "../globals.css";
+import PostCard from "../components/postCard";
+
+export default function posts() {
+  return (
+    <div className="h-full">
+        <PostCard />
+    </div>
+  );
+}
