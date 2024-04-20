ce5d4987b5e7ffe010c071ff7f34bce97a7e1278
Spencer Presley <spencerpresley96@gmail.com>
2024-04-20 14:13:43 -0400

    model plus resizing plus added particles to layout

--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -1,5 +1,6 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import React from "react"
 
 export default async function ProtectedRoute() {
   const session = await getServerSession();
