b3ca22c1cb22774c2a1d4148d225d787a54addc3
Spencer Presley <spencerpresley96@gmail.com>
2024-04-20 18:29:52 -0400

    docs, some animated gradient text

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,33 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import createMDX from '@next/mdx'
+import remarkGfm from 'remark-gfm';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeHighlight from 'rehype-highlight';
+import remarkToc from 'remark-toc';
+import remarkHtml from 'remark-html';
 
-export default nextConfig;
+// MDX configuration
+const withMDX = createMDX({
+    extension: /\.mdx?$/, // This regex will handle both .md and .mdx files
+    options: {
+        remarkPlugins: [
+            remarkGfm,
+            remarkToc,
+            remarkHtml,
+        ],
+        rehypePlugins: [
+            rehypeSlug,
+            rehypeAutolinkHeadings,
+            rehypeHighlight,
+        ],
+        providerImportSource: '@mdx-js/react',
+    },
+});
+
+// Next.js configuration
+const nextConfig = {
+    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+};
+
+// Export the configuration wrapped by withMDX
+export default withMDX(nextConfig);
