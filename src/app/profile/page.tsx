e436180af75d4a5fdd991348ecf1cab28dbfaa65
James Montebell <100632392+jamesmontebell@users.noreply.github.com>
2024-04-19 23:09:58 -0400

    added routes and basic get request for profile

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,29 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default async function ProfileDetails() {
+  const session = await getServerSession();
+  if (!session || !session.user) {
+    redirect("/api/auth/signin");
+  }
+
+  const [journals, setJournals] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = fetch("route");
+        if (!(await response).ok) {
+          throw new Error("bruh");
+        }
+        const data = await (await response).json();
+      } catch (error) {}
+    };
+    fetchData();
+
+    return () => {};
+  }, [journals]);
+
+  return <div>Profile Page {session.user.email} </div>;
+}
