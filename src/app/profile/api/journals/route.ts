e8ebea61a032d98e7c33fbbabfbb55b89c34d822
James Montebell <100632392+jamesmontebell@users.noreply.github.com>
2024-04-20 21:03:07 -0400

    saveme

--- src/app/profile/api/journals/route.ts
+++ src/app/profile/api/journals/route.ts
@@ -1,6 +1,5 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { NextResponse } from "next/server";
 
 export async function GET() {
   const session = await getServerSession();
