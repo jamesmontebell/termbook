e596e821ab2fead1cab1fcc9eeff62e8f05bf0ff
Spencer Presley <spencerpresley96@gmail.com>
2024-04-20 20:05:15 -0400

    added images and updated docs

--- src/app/components/NewNavBar.tsx
+++ src/app/components/NewNavBar.tsx
@@ -36,7 +36,7 @@ export default function NewNavBar() {
         </Link>
         <div className="flex gap-10">
           <Link href="/downloads">
-            <li className="ml-auto">Downloads</li>
+            <li className="ml-auto hover:bg-">Downloads</li>
           </Link>
           <Link href="/docs">
             <li className="ml-auto">Docs</li>
--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import '../globals.css';
 import React from "react";
 
 import { useMDXComponents } from "../../mdx-components";
@@ -9,7 +10,7 @@ export default function Page() {
   const components = useMDXComponents({});
 
   return (
-    <div className="flex flex-col min-h-screen pb-10 pt-10">
+    <div className="flex flex-col min-h-screen pb-10 pt-5 z-20 relative">
       <div className="bg-gradient-to-r from-gray-900 via-gray-950 to-black border border-cyan-700 flex shadow-lg shadow-cyan-600 mx-20 my-0.7 justify-left opacity-98 flex-grow">
         <article className="prose my-5 mx-5 text-left text-white prose-headings:text-white prose-code:text-white">
           <MDXContent components={components} />
