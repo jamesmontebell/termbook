64a0dfc9e94633d541eb856436df1ddd48a8abad
James Montebell <jamesmontebell4@gmail.com>
2024-04-22 22:28:43 -0400

    fixed downloads and arror on homepage

--- src/app/downloads/page.tsx
+++ src/app/downloads/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-import { useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
+
 import { Icon } from "@iconify/react";
 import { useEffect } from "react";
 import AOS from "aos";
@@ -9,11 +8,6 @@ import "../globals.css";
 import React from "react";
 
 export default function Downloads() {
-  // const { data: session } = useSession();
-  // if (!session || !session.user) {
-  //   redirect("/api/auth/signin");
-  // }
-
   useEffect(() => {
     AOS.init({
       disable: "phone",
@@ -23,81 +17,84 @@ export default function Downloads() {
   }, []);
 
   return (
-    <div className="flex min-h-[75vh] mb-10 max-h-screen justify-center items-center h-full flex-col gap-6">
-
-      <div className="text-5xl" data-aos="fade-down" data-aos-delay="200">
-        Download Termbook
-      </div>
-      <div data-aos="fade-down" data-aos-delay="200">
-        Free and built with love, care... and not a lot of sleep.
-      </div>
-      <div className="w-3/4 flex justify-center items-center">
+    <div className="flex h-[calc(100vh-75px] justify-center items-center flex-col gap-6 p-14">
+      <div className="p-10 flex flex-col justify-center items-center gap-6 w-3/4 ">
+        <div className="text-5xl" data-aos="fade-down" data-aos-delay="200">
+          Download Termbook
+        </div>
+        <div className="pb-6" data-aos="fade-down" data-aos-delay="200">
+          Free and built with love, care... and not a lot of sleep.
+        </div>
         <div
-          className="flex h-full p-10 w-3/4 justify-center items-center gap-[200px]"
+          className="flex h-full justify-center items-center gap-x-40  p-10"
           data-aos="fade-down"
           data-aos-delay="400"
         >
-          <div className="text-[100px]">
-            <Icon icon="devicon:windows8" />
+          <div className="flex flex-col gap-10 ">
+            <div className="text-[100px] flex justify-center items-center">
+              <Icon icon="devicon:windows8" />
+            </div>
+            <div className="flex flex-col gap-4 w-full justify-evenly">
+              <a
+                href="https://termbook.s3.amazonaws.com/termbook_windows32.exe"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md flex justify-center items-center text-center w-36 transition-colors ease-in-out"
+              >
+                Download
+                <br />
+                x32
+              </a>
+              <a
+                href="https://termbook.s3.amazonaws.com/termbook_window64.exe"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md flex justify-center items-center text-center w-36 transition-colors ease-in-out"
+              >
+                Download
+                <br />
+                x64
+              </a>
+            </div>
           </div>
-          <div className="text-[100px]">
-            <Icon icon="devicon:linux" />
+          <div className="flex flex-col gap-10">
+            <div className="text-[100px] flex justify-center items-center">
+              <Icon icon="devicon:linux" />
+            </div>
+            <div className="flex flex-col gap-4 w-full justify-evenly">
+              <a
+                href="https://termbook.s3.amazonaws.com/termbook_linux32"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md flex justify-center items-center text-center w-36 transition-colors ease-in-out"
+              >
+                Download
+                <br />
+                x32
+              </a>
+              <a
+                href="https://termbook.s3.amazonaws.com/termbook_linux64"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md flex justify-center items-center text-center w-36 transition-colors ease-in-out"
+              >
+                Download
+                <br />
+                x64
+              </a>
+            </div>
           </div>
-          <div className="text-[100px]">
-            <Icon icon="wpf:mac-os" />
+          <div className="flex flex-col gap-10">
+            <div className="text-[100px] flex justify-center items-center">
+              <Icon icon="wpf:mac-os" />
+            </div>
+            <div className="flex flex-col gap-4 w-full justify-evenly">
+              <a
+                href="https://termbook.s3.amazonaws.com/termbook_appleintel"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md flex justify-center items-center text-center w-36 transition-colors ease-in-out"
+              >
+                Download Intel Chip
+              </a>
+              <a
+                href="https://termbook.s3.amazonaws.com/termbook_applesilicon"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md flex justify-center items-center text-center w-36 transition-colors ease-in-out"
+              >
+                Download Apple Silicon
+              </a>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className="flex gap-[160px] " data-aos="fade-down">
-        <div className="flex flex-col gap-4 w-full justify-evenly">
-          <a
-            href="https://termbook.s3.amazonaws.com/termbook_windows32.exe"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md flex justify-center items-center text-center"
-          >
-            Download
-            <br />
-            x32
-          </a>
-          <a
-            href="https://termbook.s3.amazonaws.com/termbook_window64.exe"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md flex justify-center items-center text-center"
-          >
-            Download
-            <br />
-            x64
-          </a>
-        </div>
-        <div className="flex flex-col gap-4 w-full justify-around p-1">
-          <a
-            href="https://termbook.s3.amazonaws.com/termbook_linux32"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md flex justify-center items-center text-center"
-          >
-            Download
-            <br />
-            x32
-          </a>
-          <a
-            href="https://termbook.s3.amazonaws.com/termbook_linux64"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md flex justify-center items-center text-center"
-          >
-            Download
-            <br />
-            x64
-          </a>
-        </div>
-        <div className="flex flex-col gap-4 w-full justify-around">
-          <a
-            href="https://termbook.s3.amazonaws.com/termbook_appleintel"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md flex justify-center items-center text-center"
-          >
-            Download Intel Chip
-          </a>
-          <a
-            href="https://termbook.s3.amazonaws.com/termbook_applesilicon"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md flex justify-center items-center text-center"
-          >
-            Download Apple Silicon
-          </a>
         </div>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,51 +20,53 @@ export default function Home() {
 
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className="min-h-[700px] w-[95%] flex justify-center items-center h-full mb-10 text-center gap-40 p-6">
-        <div className="text-left">
-          <h1
-            className="pb-4 font-extrabold tracking-tight gradient-text text-transparent text-5xl animate-gradient"
-            data-aos="fade-down"
-          >
-            Journaling for
-          </h1>
-          <h1
-            className="pb-4 font-extrabold tracking-tight gradient-text text-transparent animate-gradient text-5xl"
-            data-aos="fade-down"
-          >
-            developers
-          </h1>
-          <p
-            className="mb-8 text-lg text-white-300/40 font-medium"
-            data-aos="fade-down"
-            data-aos-delay="200"
-          >
-            Alleviate stress through journaling, this time we WON'T let you quit
-          </p>
-          <div
-            className="flex flex-col items-center max-w-xs mx-auto gap-4 sm:max-w-none  sm:justify-center sm:flex-row sm:inline-flex"
-            data-aos="fade-down"
-            data-aos-delay="400"
-          >
-            <Link
-              className="w-full justify-center flex items-center whitespace-nowrap transition duration-150 ease-in-out font-medium rounded px-4 py-1.5  text-zinc-900 bg-white hover:bg-white/80 group"
-              href="/docs"
+      <div className="flex flex-col justify-center items-center">
+        <div className="min-h-[700px] w-[95%] flex justify-center items-center h-full text-center gap-40 p-6">
+          <div className="text-left">
+            <h1
+              className="pb-4 font-extrabold tracking-tight gradient-text text-transparent text-5xl animate-gradient"
+              data-aos="fade-down"
             >
-              Get Started{" "}
-              <ArrowRight className="w-3 h-3 tracking-normal text-primary-500 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1" />
-            </Link>
+              Journaling for
+            </h1>
+            <h1
+              className="pb-4 font-extrabold tracking-tight gradient-text text-transparent animate-gradient text-5xl"
+              data-aos="fade-down"
+            >
+              developers
+            </h1>
+            <p
+              className="mb-8 text-lg text-white-300/40 font-medium"
+              data-aos="fade-down"
+              data-aos-delay="200"
+            >
+              Alleviate stress through journaling, this time we WON'T let you
+              quit
+            </p>
+            <div
+              className="flex flex-col items-center max-w-xs mx-auto gap-4 sm:max-w-none  sm:justify-center sm:flex-row sm:inline-flex"
+              data-aos="fade-down"
+              data-aos-delay="400"
+            >
+              <Link
+                className="w-full justify-center flex items-center whitespace-nowrap transition duration-150 ease-in-out font-medium rounded px-4 py-1.5  text-zinc-900 bg-white hover:bg-white/80 group"
+                href="/docs"
+              >
+                Get Started{" "}
+                <ArrowRight className="w-3 h-3 tracking-normal text-primary-500 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1" />
+              </Link>
+            </div>
+          </div>
+          <div
+            className="font-extrabold tracking-tight text-5xl bg-clip-text "
+            data-aos="fade-down"
+          >
+            <Icon className="w-80 h-80" icon="bi:journal-text" />
           </div>
         </div>
-
-        <div
-          className="font-extrabold tracking-tight text-5xl bg-clip-text "
-          data-aos="fade-down"
-        >
-          <Icon className="w-80 h-80" icon="bi:journal-text" />
+        <div className="animate-bounce">
+          <Icon className="w-10 h-10" icon="mdi:arrow-down" />
         </div>
-      </div>
-      <div className="animate-bounce">
-        <Icon className="w-10 h-10" icon="mdi:arrow-down" />
       </div>
       <div className="pb-5 w-[90%] scroll-wrapper mt-10 mb-10 mx-auto relative">
         <Scroll />
